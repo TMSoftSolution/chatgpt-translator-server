2d16bcadac759e33bcbdb19d878b3b8fab8ffccd
Full Stack Software Engineer(Web & Mobile) <lovetallman426@gmail.com>
2023-03-03 08:26:13 -0500

    create image generator api

---
+++ pages/api/generate-image.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import openAI from "../../config";
+
+type Data = {
+  result?: string | undefined;
+  error?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (!req?.body?.query) {
+    res.status(400).json({ error: "Invalid params" });
+  }
+
+  const { query = "" } = req?.body;
+  const completion = await openAI.createImage({
+    // We are prompting the API in the below format
+    // The ?? after `query` is just a HACK
+    prompt: query,
+    n:1,
+    size: "256x256"
+ 
+  });
+  res.status(200).json({ result: completion.data.data[0].url});
+}
